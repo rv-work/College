5ca281f7c3ca5ddf2e6cea6656dc5cd455fd5fe6
rv.dthought@gmail.com <rv.dthought@gmail.com>
2025-09-03 18:23:49 +0530

    k

--- main/src/app/Context/AuthContext.tsx
+++ main/src/app/Context/AuthContext.tsx
@@ -5,20 +5,20 @@ import { createContext, ReactNode, useContext, useState } from "react";
 interface AuthContextType {
   isLoggedIn: boolean;
   setIsLoggedIn: React.Dispatch<React.SetStateAction<boolean>>;
-  goto : string;
-  setGoto : (goto : string) => void;
+  goto: string;
+  setGoto: (goto: string) => void;
 }
 
 const AuthContext = createContext<AuthContextType | null>(null);
 
 const AuthContextProvider = ({ children }: { children: ReactNode }) => {
 
-  const [isLoggedIn, setIsLoggedIn  ] = useState<boolean>(false);
-  const [goto , setGoto] = useState<string>("")
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+  const [goto, setGoto] = useState<string>("")
 
 
   return (
-    <AuthContext.Provider value ={{isLoggedIn, setIsLoggedIn ,setGoto , goto }}>
+    <AuthContext.Provider value={{ isLoggedIn, setIsLoggedIn, setGoto, goto }}>
       {children}
     </AuthContext.Provider>
   );
@@ -28,7 +28,7 @@ const AuthContextProvider = ({ children }: { children: ReactNode }) => {
 export const useAuth = () => {
   const context = useContext(AuthContext);
   if (!context) {
-    throw new Error("useAuth must be used within a useAuthProvider");
+    throw new Error("useAuth must be used within a useAuthProvider")
   }
   return context;
 };
